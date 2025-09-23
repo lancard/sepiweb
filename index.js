2967a5620c47bede7c5b31ab50b9285ea3014c6e
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-09-24 00:05:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const socketIoServer = require('socket.io').Server;
 // --------------------- express (just webserver) ---------------------
 const app = express();
 app.get('/', (req, res) => {
-    res.sendFile('/index.html');
+    res.sendFile('index.html');
 });
 
 // --------------------- http server ---------------------
@@ -57,3 +57,4 @@ io.on("connection", (socket) => {
 
 // --------------------- http server start ---------------------
 server.listen(80, '0.0.0.0');
+
